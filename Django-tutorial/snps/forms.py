1b06c5870ad4708d39ade39c54edcc97e1559b20
Jakub Giezgała <120750209+kugi8412@users.noreply.github.com>
2024-08-13 20:35:32 +0200

    Delete Django-tutorial directory

--- Django-tutorial/snps/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Animal
-
-class AnimalForm(forms.ModelForm):
-    class Meta:
-        model = Animal
-        fields = ["name", "image"]
--- Django-tutorial/snps/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("search-snps", views.search_snps, name="search_snps"),
-    path("create-animal/", views.create_animal, name="create_animal"),
-]
